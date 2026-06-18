7232b1990f55fb9ddf6398134f26d38fe540a786
tara <losronda2000@gmail.com>
2026-06-17 23:45:13 -0400

    Revise news settings in site-settings.js
    Updated news section with new title, body, image, and accent color.

--- site-settings.js
+++ site-settings.js
@@ -1,8 +1,8 @@
 window.SWEET16_SETTINGS = {
   "siteTitle": "Sweet 16 Charts",
   "siteTagline": "The official weekly music chart",
-  "newsTitle": "Sweet 16 News",
-  "newsBody": "Latest chart news will appear here.",
-  "newsImage": "",
-  "accentColor": ""
+  "newsTitle": "Olivia Domination",
+  "newsBody": "Olivia Rodrigo's you seem pretty sad for a girl so in love, becomes the biggest album debut of 2026.\nThe artist also achieve a record-breaking 6th #1 debut, the first in history to do so.",
+  "newsImage": "https://www.billboard.com/wp-content/uploads/2026/06/olivia-rodrigo-nick-walker-billboard-1800.jpg?w=942&h=628&crop=1",
+  "accentColor": "#f4a4c0"
 };
